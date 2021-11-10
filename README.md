fa7b10b71827604524121d4729c70b6c70b4a1e1
小源 <511568816@qq.com>
2021-11-10 21:33:01 +0800

    cpu-api

---
+++ README.md
@@ -0,0 +1,15 @@
+# ostep
+
+## TODO
+
+cpu-api/p4
+cpu-sched-lottery
+dist-intro
+intro
+threads-api
+threads-bugs
+threads-cv
+threads-intro
+threads-locks
+threads-sema
+vm-intro
---
+++ user-lib/src/lib.rs
@@ -0,0 +1,27 @@
+extern crate nix;
+
+pub use nix::errno::Errno;
+pub use nix::unistd::{ForkResult, Pid};
+pub use nix::sys::wait::{self, WaitStatus};
+use std::convert::Infallible;
+use std::ffi::CStr;
+
+pub fn fork() -> Result<ForkResult, Errno> {
+    unsafe { nix::unistd::fork() }
+}
+
+pub fn getpid() -> Pid{
+    nix::unistd::getpid()
+}
+
+pub fn wait() -> Result<WaitStatus, Errno>{
+    nix::sys::wait::wait()
+}
+
+pub fn sleep(seconds: u32) -> u32 {
+    nix::unistd::sleep(seconds)
+}
+
+pub fn execvp<S: AsRef<CStr>>(filename: &CStr, args: &[S]) -> Result<Infallible, Errno> {
+    nix::unistd::execvp(filename, args)
+}
